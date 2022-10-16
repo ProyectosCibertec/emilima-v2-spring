825b3da0eedb7aa5172363301e7b441117397bc9
quispewilmer <wilmerquispecode@gmail.com>
2022-10-16 19:44:04 +0000

    Optimize imports

--- src/main/java/pe/com/emilima/dms/model/UserRole.java
+++ src/main/java/pe/com/emilima/dms/model/UserRole.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.io.Serializable;
 import java.math.BigInteger;
 
 @Entity
--- src/main/java/pe/com/emilima/dms/repository/UserRepository.java
+++ src/main/java/pe/com/emilima/dms/repository/UserRepository.java
@@ -2,10 +2,7 @@ package pe.com.emilima.dms.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import pe.com.emilima.dms.model.RequestState;
 import pe.com.emilima.dms.model.User;
-
-import java.math.BigInteger;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {

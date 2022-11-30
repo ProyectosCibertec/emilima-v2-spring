fb29727f3630549afdca4d73d3ec7c0dd0186054
quispewilmer <wilmerquispecode@gmail.com>
2022-11-30 16:38:31 -0500

    Generate reports successfully

--- src/main/java/pe/com/emilima/dms/controller/api/documentalserie/ApiDocumentalSerieController.java
+++ src/main/java/pe/com/emilima/dms/controller/api/documentalserie/ApiDocumentalSerieController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pe.com.emilima.dms.model.DocumentalSerie;
 import pe.com.emilima.dms.service.DocumentalSerieService;
+import pe.com.emilima.dms.service.ReportService;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -12,6 +15,8 @@ import java.util.Optional;
 public class ApiDocumentalSerieController {
     @Autowired
     private DocumentalSerieService documentalSerieService;
+    @Autowired
+    private ReportService<DocumentalSerie> reportService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public Iterable<DocumentalSerie> list() {
@@ -31,5 +36,10 @@ public class ApiDocumentalSerieController {
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable("id") String id) {
         documentalSerieService.deleteById(id);
+    }
+
+    @RequestMapping(value = "/report", method = RequestMethod.GET)
+    public void generateReport(HttpServletResponse response) {
+        reportService.generateReportAndDownload(response, "base/reporte-series-documentales.jrxml", (List<DocumentalSerie>) documentalSerieService.findAll());
     }
 }
--- src/main/java/pe/com/emilima/dms/controller/api/documentrequest/ApiDocumentRequestController.java
+++ src/main/java/pe/com/emilima/dms/controller/api/documentrequest/ApiDocumentRequestController.java
@@ -3,9 +3,13 @@ package pe.com.emilima.dms.controller.api.documentrequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pe.com.emilima.dms.model.DocumentRequest;
+import pe.com.emilima.dms.model.DocumentalSerie;
 import pe.com.emilima.dms.service.DocumentRequestService;
+import pe.com.emilima.dms.service.ReportService;
 
+import javax.servlet.http.HttpServletResponse;
 import java.math.BigInteger;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -13,6 +17,8 @@ import java.util.Optional;
 public class ApiDocumentRequestController {
     @Autowired
     private DocumentRequestService documentRequestService;
+    @Autowired
+    private ReportService<DocumentRequest> reportService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public Iterable<DocumentRequest> list() {
@@ -32,5 +38,10 @@ public class ApiDocumentRequestController {
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable("id") BigInteger id) {
         documentRequestService.deleteById(id);
+    }
+
+    @RequestMapping(value = "/report", method = RequestMethod.GET)
+    public void generateReport(HttpServletResponse response) {
+        reportService.generateReportAndDownload(response, "base/reporte-solicitudes-documentales.jrxml", (List<DocumentRequest>) documentRequestService.findAll());
     }
 }
